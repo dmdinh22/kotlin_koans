3622579d4558f4eeec77bc18b5bf9530afef0840
David Dinh <dmdinh22@gmail.com>
2020-02-17 00:54:22 -0800

    ranges

--- Conventions/Ranges/src/Task.kt
+++ Conventions/Ranges/src/Task.kt
@@ -1,3 +1,9 @@
+class DateRange(val start: MyDate, val endInclusive: MyDate) {
+    operator fun contains(item: MyDate): Boolean {
+        return start <= item && item <= endInclusive
+    }
+}
+
 fun checkInRange(date: MyDate, first: MyDate, last: MyDate): Boolean {
-    return TODO()
+    return date in DateRange(first, last)
 }
